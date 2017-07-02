d4cb6d1709038ac5a6f4e2aa4191f604a313c633
pookzzz2231 <w.kuttiya@gmail.com>
2017-07-02 00:58:39 -0400

    stars ui

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,5 @@
 class ReviewsController < ApplicationController
-  
+  def index
+    @reviews = Review.limit(10)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :business
+
+  default_scope { order('created_at DESC') }
+
+  def user_name
+    self.user.first_name
+  end
+
+  def business_name
+    self.business.name
+  end
 end
